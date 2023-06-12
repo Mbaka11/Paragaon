d0bcf02e00551b49079571b98f539db4594abab0
Mbaka11 <90229093+Mbaka11@users.noreply.github.com>
2023-06-12 15:08:29 -0400

    starting files

---
+++ client/src/app/modules/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: MainPageComponent },
+    { path: 'material', component: MaterialPageComponent },
+    { path: '**', redirectTo: '/home' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
