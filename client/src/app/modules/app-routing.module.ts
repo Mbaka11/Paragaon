e846cd5b14ad39c455802465e05114437974b118
Wizxou <68761094+Wizxou@users.noreply.github.com>
2023-06-13 17:04:08 -0400

    forfait page cree

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForfaitPageComponent } from '@app/pages/forfait-page/forfait-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
-    { path: 'material', component: MaterialPageComponent },
+    { path: 'material', component: MaterialPageComponent }, // TODO: enlever cette page
+    { path: 'forfaits', component: ForfaitPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
---
+++ client/src/app/pages/forfait-page/forfait-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forfait-page',
+  templateUrl: './forfait-page.component.html',
+  styleUrls: ['./forfait-page.component.scss']
+})
+export class ForfaitPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
