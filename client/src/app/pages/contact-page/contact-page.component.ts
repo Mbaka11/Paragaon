24476fb65cc1387041d406209d6ae68a43aae2a9
Wizxou <68761094+Wizxou@users.noreply.github.com>
2023-06-17 03:57:19 -0400

    contact page started

--- client/src/app/pages/contact-page/contact-page.component.ts
+++ client/src/app/pages/contact-page/contact-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-page',
-  templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss']
+    selector: 'app-contact-page',
+    templateUrl: './contact-page.component.html',
+    styleUrls: ['./contact-page.component.scss'],
 })
-export class ContactPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ContactPageComponent implements AfterViewInit {
+    ngAfterViewInit() {
+        const script = document.createElement('script');
+        script.src = 'https://static.elfsight.com/platform/platform.js';
+        script.setAttribute('data-use-service-core', '');
+        script.defer = true;
+        document.body.appendChild(script);
+    }
 }
