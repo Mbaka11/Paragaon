0fec49f3f1125f410ad932202de4c9fee12f187e
Wassim Hamadache <wizxou@gmail.com>
2023-10-09 15:06:37 -0400

    forfait page btn hover

--- client/src/app/pages/forfait-page/forfait-page.component.ts
+++ client/src/app/pages/forfait-page/forfait-page.component.ts
@@ -16,23 +16,39 @@ export class ForfaitPageComponent {
         this.isForfait2 = false;
         this.isForfait3 = false;
         this.isForfait4 = false;
+        this.changeHoverButton(1);
     }
     changeForfait2() {
         this.isForfait1 = false;
         this.isForfait2 = true;
         this.isForfait3 = false;
         this.isForfait4 = false;
+        this.changeHoverButton(2);
     }
     changeForfait3() {
         this.isForfait1 = false;
         this.isForfait2 = false;
         this.isForfait3 = true;
         this.isForfait4 = false;
+        this.changeHoverButton(3);
     }
     changeForfait4() {
         this.isForfait1 = false;
         this.isForfait2 = false;
         this.isForfait3 = false;
         this.isForfait4 = true;
+        this.changeHoverButton(4);
+    }
+
+    changeHoverButton(btnNumber: number) {
+        const element = document.getElementById(`btn-${btnNumber}`);
+        element?.classList.add('btn-hover');
+
+        for (let i = 1; i < 5; i++) {
+            if (i !== btnNumber) {
+                const otherElement = document.getElementById(`btn-${i}`);
+                otherElement?.classList.remove('btn-hover');
+            }
+        }
     }
 }
